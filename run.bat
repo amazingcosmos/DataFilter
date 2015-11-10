6c95d6e94a43445a11604bd904636cfb8b9d4b11
unknown <>
2015-11-10 21:00:18 +0800

    add run.bat run.sh

---
+++ run.bat
@@ -0,0 +1,4 @@
+echo "Make sure you are online! It takes a few seconds to install."
+@pip install chardet
+@python filter.py
+@set/p option=enter anykey to finish.
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+hello = "Make sure you are online! It takes a few seconds to install."
+echo $hello
+pip install chardet
+python filter.py
