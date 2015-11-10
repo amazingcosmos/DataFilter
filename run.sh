c6897b8e367b92dcc5b7b8b70c3cd84fc9127777
unknown <>
2015-11-10 21:10:33 +0800

    update run.sh echo

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-hello="Make sure you are online! It takes a few seconds to install."
-echo $hello
+echo "Make sure you are online! It takes a few seconds to install."
 pip install chardet
 python filter.py
